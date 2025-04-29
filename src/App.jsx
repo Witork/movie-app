e52126613f959a13f91f04465c7ca793f0e332cc
Witork <costin.mazilu89@gmail.com>
2025-04-29 14:44:06 +0300

    Advanced search

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,37 @@ import { useEffect, useState } from 'react';
 import Search from './components/Search';
 import Spinner from './components/Spinner';
 import MovieCard from './components/MovieCard';
+import { useDebounce } from 'react-use';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
-
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
-
 const API_OPTIONS = {
   method: 'GET',
   headers: {
-    accept: 'application/json', 
+    accept: 'application/json',
     Authorization: `Bearer ${API_KEY}`
   }
 };
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState(''); // Separate state for debounced term
   const [errorMessage, setErrorMessage] = useState('');
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  const fetchMovies = async () => {
+  useDebounce(() => setDebouncedSearchTerm(searchTerm), 500 ,[searchTerm]);
+
+  const fetchMovies = async (query = '') => {
     setIsLoading(true);
     setErrorMessage('');
     try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}`;
+      const endpoint = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
       const response = await fetch(endpoint, API_OPTIONS);
+
       if (!response.ok) {
         throw new Error(`Failed to fetch movies`);
       }
@@ -46,8 +52,8 @@ const App = () => {
   };
 
   useEffect(() => {
-    fetchMovies();
-  }, []);
+    fetchMovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
 
   return (
     <main>
@@ -60,7 +66,6 @@ const App = () => {
         </header>
         <section className='all-movies'>
           <h2 className="mt-[40px]">All Movies</h2>
-
           {isLoading ? (
             <Spinner />
           ) : errorMessage ? (
@@ -72,14 +77,13 @@ const App = () => {
                   key={movie.id}
                   movie={movie}
                 />
-
               ))}
             </ul>
           )}
         </section>
       </div>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
